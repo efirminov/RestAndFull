381f2d10c13cdb994dda832578bf27fcc04ed02c
Do Vale Firmino <edov@minsait.com>
2022-07-18 12:32:13 -0300

    content Negotiation

---
+++ 13_ConteudoNegociacao/src/main/java/br/com/github/efirminov/restandfull/repositories/PersonRepository.java
@@ -0,0 +1,8 @@
+package br.com.github.efirminov.restandfull.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.github.efirminov.restandfull.model.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
